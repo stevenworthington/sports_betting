7a8b5a1666668dd67b1e9f97453a0409006b51e8
Russell Brown <r.n.brown314@gmail.com>
2024-02-19 18:08:24 -0500

    odds data and betting simulation

---
+++ code/betting/betting_strategies.py
@@ -0,0 +1,233 @@
+from datetime import datetime
+import pandas as pd
+from dotenv import load_dotenv, find_dotenv
+import os
+from random import randint
+from wagers import bet_wins
+
+load_dotenv(find_dotenv())
+REPO_ROOT = os.environ['REPO_ROOT']
+DATA = f'{REPO_ROOT}/data/prop_odds_api'
+
+moneyline_df = pd.read_csv(f'{DATA}/moneyline.csv')
+spread_df = pd.read_csv(f'{DATA}/spread.csv')
+games_df = pd.read_csv(f'{DATA}/prop_odds_games.csv')
+
+def casual_fan(start_date, end_date, team_name):
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+
+    games = games_df[(games_df.date>=start_date.strftime('%Y-%d-%m')) & (games_df.date<end_date.strftime('%Y-%m-%d'))]
+    games = games[(games.away_team==team_name) | (games.home_team==team_name)].sort_values('date')[['game_id', 'date']]
+
+    odds = spread_df[(spread_df.bookie=='betmgm') & (spread_df.name==team_name)]
+    odds = pd.merge(games, odds, left_on='game_id', right_on='game_id')
+    odds = odds[~odds.game_id.duplicated()]
+
+    wager_history = {
+        'bankroll': 1000,
+        'betting_units': 'dollars', # or dollars
+        'wagers': [
+            {
+                'type':'single', 
+                'amount':1, 
+                'bet': {
+                    'market': 'spread',
+                    'date': row.date,
+                    'handicap': row.handicap,
+                    'odds': row.odds,
+                    'team_name': row['name']
+                }
+            }
+            for i, row in odds.iterrows()
+        ]
+    }
+
+    return wager_history
+
+def casual_fan_moneyline(start_date, end_date, team_name):
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+
+    games = games_df[(games_df.date>=start_date.strftime('%Y-%d-%m')) & (games_df.date<end_date.strftime('%Y-%m-%d'))]
+    games = games[(games.away_team==team_name) | (games.home_team==team_name)].sort_values('date')[['game_id', 'date']]
+
+    odds = moneyline_df[(moneyline_df.bookie=='betmgm') & (moneyline_df.name==team_name)]
+    odds = pd.merge(games, odds, left_on='game_id', right_on='game_id')
+    odds = odds[~odds.game_id.duplicated()]
+
+    wager_history = {
+        'bankroll': 1000,
+        'betting_units': 'dollars', # or dollars
+        'wagers': [
+            {
+                'type':'single', 
+                'amount':10, 
+                'bet': {
+                    'market': 'moneyline',
+                    'date': row.date,
+                    'handicap': row.handicap,
+                    'odds': row.odds,
+                    'team_name': row['name']
+                }
+            }
+            for i, row in odds.iterrows()
+        ]
+    }
+
+    return wager_history
+
+def degen_fan(start_date, end_date, team_name):
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+
+    games = games_df[(games_df.date>=start_date.strftime('%Y-%d-%m')) & (games_df.date<end_date.strftime('%Y-%m-%d'))]
+    games = games[(games.away_team==team_name) | (games.home_team==team_name)].sort_values('date')[['game_id', 'date']]
+
+    odds = spread_df[(spread_df.bookie=='betmgm') & (spread_df.name==team_name)]
+    odds = pd.merge(games, odds, left_on='game_id', right_on='game_id')
+    odds = odds[~odds.game_id.duplicated()]
+
+    wager_history = {
+        'bankroll': 1000,
+        'betting_units': 'units', # or dollars
+        'wagers': [
+            {
+                'type':'single', 
+                'amount':10, 
+                'bet': {
+                    'market': 'spread',
+                    'date': row.date,
+                    'handicap': row.handicap,
+                    'odds': row.odds,
+                    'team_name': row['name']
+                }
+            }
+            for i, row in odds.iterrows()
+        ]
+    }
+
+    return wager_history
+
+def pick_random(start_date, end_date, units, amount, frac):
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+
+    games = games_df[(games_df.date>=start_date.strftime('%Y-%d-%m')) & (games_df.date<end_date.strftime('%Y-%m-%d'))]
+    games = games.sort_values('date')[['game_id', 'date', 'home_team', 'away_team']]
+    games = games.sample(frac=frac)
+    
+    odds = spread_df[(spread_df.bookie=='betmgm')]
+    odds = pd.merge(games, odds, left_on='game_id', right_on='game_id')
+    odds = odds.groupby('game_id').apply(lambda df: df.sample(1))
+
+    wager_history = {
+        'bankroll': 1000,
+        'betting_units': units, # or dollars
+        'wagers': [
+            {
+                'type':'single', 
+                'amount':amount, 
+                'bet': {
+                    'market': 'spread',
+                    'date': row.date,
+                    'handicap': row.handicap,
+                    'odds': row.odds,
+                    'team_name': row['name']
+                }
+            }
+            for i, row in odds.iterrows()
+        ]
+    }
+
+    return wager_history
+
+def parlay_demon(start_date, end_date, units, amount):
+    #one parlay per day, 2-x
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+
+    games = games_df[(games_df.date>=start_date.strftime('%Y-%d-%m')) & (games_df.date<end_date.strftime('%Y-%m-%d'))]
+    games = games.sort_values('date')[['game_id', 'date', 'home_team', 'away_team']]
+    
+    def pick_parlays(df):
+        df = df[~df['name'].duplicated()]
+        n = len(df)
+        if n < 2:
+            return []
+        
+        legs = randint(2, n)
+        df = df.sample(n=max(legs, n))
+        
+        return [
+            {
+                'market': 'spread',
+                'date': row.date,
+                'handicap': row.handicap,
+                'odds': row.odds,
+                'team_name': row['name']
+            }
+            for i, row in df.iterrows()
+        ]
+        
+    odds = spread_df[(spread_df.bookie=='betmgm')]
+    odds = pd.merge(games, odds, left_on='game_id', right_on='game_id')
+    parlays = odds.groupby('game_id').apply(pick_parlays).values
+    
+    wager_history = {
+        'bankroll': 1000,
+        'betting_units': units,
+        'wagers': [
+            {
+                'type':'parlay', 
+                'amount':amount, 
+                'legs': parlays[i]
+            }
+            for i in range(len(parlays))
+        ]
+    }
+
+    return wager_history
+
+def parlay_god(start_date, end_date, units, amount):
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+
+    games = games_df[(games_df.date>=start_date.strftime('%Y-%d-%m')) & (games_df.date<end_date.strftime('%Y-%m-%d'))]
+    games = games.sort_values('date')[['game_id', 'date', 'home_team', 'away_team']]
+    
+    def pick_parlays(df):
+        winning_bets = []
+        already_bet = []
+        
+        for i, row in df.iterrows():
+            bet = {
+                'market': 'spread',
+                'date': row.date,
+                'handicap': row.handicap,
+                'odds': row.odds,
+                'team_name': row['name']
+            }
+            if bet_wins(bet) and not (row['name'] in already_bet):
+                winning_bets.append(bet)
+                already_bet.append(row['name'])
+        return winning_bets
+
+    odds = spread_df[(spread_df.bookie=='betmgm')]
+    odds = pd.merge(games, odds, left_on='game_id', right_on='game_id')
+    parlays = odds.groupby('game_id').apply(pick_parlays).values
+    
+    wager_history = {
+        'bankroll': 1000,
+        'betting_units': units,
+        'wagers': [
+            {
+                'type':'parlay', 
+                'amount':amount, 
+                'legs': parlays[i]
+            }
+            for i in range(len(parlays))
+        ]
+    }
+
+    return wager_history
---
+++ code/betting/get_prop_odds_data.py
@@ -0,0 +1,180 @@
+import requests
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import json
+import pandas as pd
+import os
+from collections import defaultdict
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+PROP_ODDS_API_KEY = os.environ['PROP_ODDS_API_KEY']
+REPO_ROOT = os.environ['REPO_ROOT']
+
+DATA = f'{REPO_ROOT}/data/prop_odds_api'
+MONEYLINE = f'{DATA}/moneyline.csv'
+PLAYER_PAR = f'{DATA}/player_par.csv'
+SPREAD = f'{DATA}/spread.csv'
+GAMES = f'{DATA}/prop_odds_games.csv'
+
+def prop_odds_api_req(endpoint, **kwargs):
+    base_url = 'https://api.prop-odds.com'
+    params = {'api_key': PROP_ODDS_API_KEY, 'tz':'America/New_York'}
+    params.update(kwargs)
+    x = requests.get(f'{base_url}{endpoint}', params=params)
+    return x
+
+
+def extract_moneyline(x):
+    gid = x['game_id']
+    
+    res = []
+    for s in x['odds'].get('sportsbooks', []):
+        bookie = s['bookie_key']
+        for o in s['market']['outcomes']:
+            res.append(
+                (gid, bookie, o['timestamp'], o['handicap'], o['odds'], o['name'])
+            )
+            
+    pd.DataFrame(
+        res, 
+        columns=['game_id', 'bookie', 'timestamp', 'handicap', 'odds', 'name']
+    ).to_csv(
+        MONEYLINE, 
+        index=None, 
+        mode='a+',
+        header = not os.path.exists(MONEYLINE)
+    )
+
+    
+def extract_spread(x):
+    gid = x['game_id']
+    
+    res = []
+    for s in x['odds'].get('sportsbooks', []):
+        bookie = s['bookie_key']
+        for o in s['market']['outcomes']:
+            res.append(
+                (gid, bookie, o['timestamp'], o['handicap'], o['odds'], o['name'])
+            )
+            
+    pd.DataFrame(
+        res, 
+        columns=['game_id', 'bookie', 'timestamp', 'handicap', 'odds', 'name']
+    ).to_csv(
+        SPREAD, 
+        index=None, 
+        mode='a+',
+        header = not os.path.exists(SPREAD)
+    )
+
+    
+def extract_par(x):
+    gid = x['game_id']
+    
+    res = []
+    for s in x['odds'].get('sportsbooks', []):
+        bookie = s['bookie_key']
+        for o in s['market']['outcomes']:
+            res.append(
+                (gid, bookie, o['timestamp'], o['handicap'], o['odds'], o['name'], o['participant'], o['participant_name'])
+            )
+            
+    pd.DataFrame(
+        res, 
+        columns=['game_id', 'bookie', 'timestamp', 'handicap', 'odds', 'name', 'participant', 'participant_name']
+    ).to_csv(
+        PLAYER_PAR, 
+        index=None, 
+        mode='a+',
+        header = not os.path.exists(PLAYER_PAR)
+    )
+
+    
+def extract_odds(x):
+    if x['market'] == 'moneyline':
+        return extract_moneyline(x)
+    if x['market'] == 'spread':
+        return extract_spread(x)
+    if x['market'] == 'player_assists_points_rebounds_over_under':
+        return extract_par(x)
+    
+
+def get_odds_data(start_date, end_date):
+    games_df = pd.read_csv(GAMES)
+    seen_dates = games_df.date.unique()
+
+    games = []
+    
+    while start_date < end_date:
+        if start_date.strftime('%Y-%m-%d') not in seen_dates:
+            res = prop_odds_api_req('/beta/games/nba', date=start_date.strftime('%Y-%m-%d'))
+            games.append((res.status_code, json.loads(res.text)))
+        start_date = start_date + relativedelta(days=1)
+
+    new_game_ids = []
+    for x in games:
+        if x[0] != 200:
+            print(x)
+            continue
+
+        all_games = []
+        date = x[1]['date']
+        for g in x[1]['games']:
+            all_games.append((date, g['game_id'], g['away_team'], g['home_team'], g['start_timestamp']))
+
+        temp_df = pd.DataFrame(
+            all_games,
+            columns = ['date', 'game_id', 'away_team', 'home_team', 'start_timestamp']
+        )
+
+        temp_df.to_csv(
+            GAMES,
+            mode = 'a+',
+            index = None,
+            header = not os.path.exists(GAMES)
+        )
+
+        new_game_ids.extend(temp_df.game_id.values)
+
+    games_df = pd.read_csv(GAMES)
+    seen_game_ids = games_df.game_id.unique()
+
+    # get odds from prop odds api
+
+    moneyline_df = pd.read_csv(MONEYLINE)
+    par_df = pd.read_csv(PLAYER_PAR)
+    spread_df = pd.read_csv(SPREAD)
+
+    odds_game_ids = list(set(
+        list(moneyline_df.game_id.unique()) + 
+        list(par_df.game_id.unique()) + 
+        list(spread_df.game_id.unique())
+    ))
+
+    odds = []
+
+    for game_id in (set(seen_game_ids) - set(odds_game_ids)):
+        for market in ['moneyline', 'spread', 'player_assists_points_rebounds_over_under']:
+            res = prop_odds_api_req(f'/v1/odds/{game_id}/{market}')
+            odds.append((game_id, market, res))
+
+    if len(odds) > 0:
+        with open(f'{DATA}/prop_odds_odds.json', 'w+') as f:
+            temp = {
+                'odds':[
+                    {'game_id':x[0], 'market':x[1], 'odds':json.loads(x[2].text)} 
+                    for x in odds
+                ]
+            }
+            f.write(json.dumps(temp))
+
+        for x in temp['odds']:
+            extract_odds(x)
+
+start_date = datetime.strptime('2023-08-01', '%Y-%m-%d')
+end_date = datetime.strptime('2024-02-20', '%Y-%m-%d')
+get_odds_data(start_date, end_date)
+                  
+      
+                
